7dd1239a2d12d4c163f667cc4a7f6c36e444ad39
HilmiMithwa <hilmimithwa@gmail.com>
2026-03-18 21:05:07 +0700

    mobile: skills section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import "./components/MenuButtonNavbar";
 import MenuButtonNavbar from './components/MenuButtonNavbar';
 import fotoHilmi from "./images/foto-hilmi.png";
 import TypeWriter from 'typewriter-effect';
+import reactImage from './images/mdi_react.png'
+import figmaImage from './images/solar_figma-bold.png'
+import pythonImage from './images/akar-icons_python-fill.png'
+import javascriptImage from './images/ri_javascript-fill.png'
+import tailwindImage from './images/lineicons_tailwindcss.png'
+import nextImage from './images/next-js.png'
 
 function App() {
   return(
@@ -37,7 +43,37 @@ function App() {
 
           <div className="min-[428px]:pt-8.75">
             <h1 className="font-semibold text-[60px]">Skills</h1>
-            <div>
+            <div className="flex flex-wrap justify-center gap-8.75 items-center ">
+
+              <div className="border-white border-3 flex flex-col items-center w-89.75 h-47 justify-center rounded-[15px]">
+                <img src={reactImage}></img>
+                <p className="text-[25px]">React</p>
+              </div>
+
+              <div className="border-white border-3 flex flex-col items-center w-89.75 h-47 justify-center rounded-[15px]">
+                <img src={figmaImage}></img>
+                <p className="text-[25px]">Figma</p>
+              </div>
+
+              <div className="border-white border-3 flex flex-col items-center w-89.75 h-47 justify-center rounded-[15px]">
+                <img src={pythonImage}></img>
+                <p className="text-[25px]">Python</p>
+              </div>
+
+              <div className="border-white border-3 flex flex-col items-center w-89.75 h-47 justify-center rounded-[15px]">
+                <img src={javascriptImage}></img>
+                <p className="text-[25px]">Javascript</p>
+              </div>
+
+              <div className="border-white border-3 flex flex-col items-center w-89.75 h-47 justify-center rounded-[15px]">
+                <img src={tailwindImage}></img>
+                <p className="text-[25px]">Tailwind CSS</p>
+              </div>
+
+              <div className="border-white border-3 flex flex-col items-center w-89.75 h-47 justify-center rounded-[15px]">
+                <img src={nextImage}></img>
+                <p className="text-[25px]">Next.Js</p>
+              </div>
               
             </div>
           </div>
--- src/components/MenuButtonNavbar.jsx
+++ src/components/MenuButtonNavbar.jsx
@@ -6,11 +6,9 @@ export default function MenuButtonNavbar() {
   return (
     <nav className="p-4 bg-black shadow-md relative">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
-        {/* LOGO */}
+
         <div className="text-xl font-roboto text-white font-bold">Hilmi Mithwa</div>
 
-        {/* --- 1. MENU DESKTOP --- */}
-        {/* Muncul hanya jika layar >= 428px */}
         <ul className="hidden min-[430px]:flex space-x-8 text-white font-medium">
           <li><a href="#" className="hover:text-gray-400 transition">About Me</a></li>
           <li><a href="#" className="hover:text-gray-400 transition">Skills</a></li>
@@ -18,8 +16,7 @@ export default function MenuButtonNavbar() {
           <li><a href="#" className="hover:text-gray-400 transition">Social Media</a></li>
         </ul>
 
-        {/* --- 2. BURGER BUTTON (MOBILE ONLY) --- */}
-        {/* Hilang jika layar >= 428px */}
+
         <button 
           onClick={() => setIsOpen(!isOpen)}
           className="flex flex-col justify-center items-center w-10 h-10 space-y-1.5 focus:outline-none z-50 min-[430px]:hidden"
@@ -30,8 +27,7 @@ export default function MenuButtonNavbar() {
         </button>
       </div>
 
-      {/* --- 3. MOBILE MENU PANEL --- */}
-      {/* PERBAIKAN: Gunakan min-[428px]:hidden agar panel ini lenyap di layar lebar */}
+
       <div className={`fixed top-0 right-0 h-full w-64 bg-zinc-900 shadow-2xl transform transition-transform duration-300 ease-in-out z-40 min-[430px]:hidden ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}>
         <ul className="flex flex-col p-10 pt-20 space-y-6 text-white text-lg">
           <li><a onClick={() => setIsOpen(false)} href="#" className="hover:text-gray-400">About Me</a></li>
